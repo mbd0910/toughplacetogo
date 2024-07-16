72dd3f7900c61340dd9f043952ec291a3e3d5fd9
Michael Davis <745953+mbd0910@users.noreply.github.com>
2024-07-16 11:03:58 +0100

    Games between teams

--- app/main.py
+++ app/main.py
@@ -1,27 +1,45 @@
-from models import db, Team
+from models import db, Team, Game, GameTeam
+from datetime import datetime
 
 
 def init_db():
     db.connect()
-    db.create_tables([Team])
+    db.create_tables([Team, Game, GameTeam])
 
 
 def add_team(name):
     team = Team(name=name)
     team.save()
+    return team
+
+
+def add_game(kickoff, team1, team2):
+    game = Game.create(kickoff=kickoff)
+    GameTeam.create(number=1, team=team1, game=game)
+    GameTeam.create(number=2, team=team2, game=game)
+    return game
 
 
 def get_teams():
     return Team.select()
 
 
+def get_games():
+    return Game.select().prefetch(GameTeam, Team)
+
+
 if __name__ == '__main__':
     init_db()
 
-    add_team('Charlton Athletic')
-    add_team('Notts County')
+    cafc = add_team('Charlton Athletic')
+    notts = add_team('Notts County')
 
-    # Fetching and displaying users
+    game = add_game(datetime(year=2024, month=7, day=16, hour=15, minute=0, second=0), cafc, notts)
+
     teams = get_teams()
     for team in teams:
         print(f'Team {team.id}: {team.name}')
+
+    games = get_games()
+    for game in games:
+        print(f'Game kicking off at {game.kickoff} between {game.home_team().name} and {game.away_team().name}')
--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from peewee import CharField, DateTimeField, PostgresqlDatabase
+from peewee import CharField, DateTimeField, PostgresqlDatabase, ForeignKeyField, IntegerField
 from playhouse.signals import pre_save, Model
 from config import DATABASE
 
@@ -24,6 +24,28 @@ class TimestampableModel(BaseModel):
 
 class Team(TimestampableModel):
     name = CharField()
+
+
+class Game(TimestampableModel):
+    kickoff = DateTimeField()
+
+    def home_team(self):
+        return self.team(1)
+
+    def away_team(self):
+        return self.team(2)
+
+    def team(self, number):
+        for game_team in self.game_teams:
+            if game_team.number == number:
+                return game_team.team
+        raise Exception(f'Cannot find game team with index {number}')
+
+
+class GameTeam(TimestampableModel):
+    number = IntegerField()
+    team = ForeignKeyField(Team, backref='game_teams')
+    game = ForeignKeyField(Game, backref='game_teams')
 
 
 @pre_save(sender=TimestampableModel)
