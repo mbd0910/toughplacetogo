cf7c74cf31f7c05bf767d8455e67b1a672ef849c
Michael Davis <745953+mbd0910@users.noreply.github.com>
2024-08-06 11:54:10 +0100

    Replaced json string manual creation with Python objects and json.dumps

--- app/betfair_login.py
+++ app/betfair_login.py
@@ -21,6 +21,14 @@ headers = {
 
 url_prefix = 'https://api.betfair.com/exchange/betting/rest/v1.0/'
 
+PREMIER_LEAGUE_ID = 10932509
+CHAMPIONSHIP_ID = 7129730
+LEAGUE_ONE_ID = 35
+LEAGUE_TWO_ID = 37
+NATIONAL_LEAGUE_ID = 11086347
+
+FOOTBALL_EVENT_ID = 1
+
 # Perform the login request
 try:
     response = requests.post(login_url, data=payload, cert=(cert_file, key_file), headers=headers)
@@ -34,23 +42,67 @@ try:
         print(f"Login successful! Session Token: {session_token}")
         print(headers)
 
-        football_and_friday_filter = '{"filter":{ "eventTypeIds": ["1"], "marketStartTime": {"from": "2024-08-05T00:00:00Z","to": "2024-08-05T23:59:59Z"}}}'
+        football_and_weekend_filter = json.dumps({
+            "filter": {
+                "eventTypeIds": [FOOTBALL_EVENT_ID],
+                "marketStartTime": {
+                    "from": "2024-08-09T00:00:00Z",
+                    "to": "2024-08-12T23:59:59Z"
+                }
+            }
+        })
+        football_weekend_and_competition_filter = json.dumps({
+            "filter": {
+                "eventTypeIds": [FOOTBALL_EVENT_ID],
+                "competitionIds": [CHAMPIONSHIP_ID, LEAGUE_ONE_ID, LEAGUE_TWO_ID],
+                "marketStartTime": {
+                    "from": "2024-08-09T00:00:00Z",
+                    "to": "2024-08-12T23:59:59Z"
+                }
+            }
+        })
+
+        list_competitions_endpoint = url_prefix + 'listCompetitions/'
         list_events_endpoint = url_prefix + 'listEvents/'
 
-        response = requests.post(list_events_endpoint, data=football_and_friday_filter, headers=headers)
-        print(json.dumps(json.loads(response.text), indent=3))
+        response = requests.post(list_events_endpoint, data=football_weekend_and_competition_filter, headers=headers)
+        json_response = sorted(json.loads(response.text), key=lambda x: x['event']['openDate'])
+        print(json.dumps(json_response, indent=3))
+        output = "\n".join([f"{event['event']['id']},{event['event']['name']}" for event in json_response])
 
-        # individual_filter = '{"filter":{ "eventIds": ["33464179"], "marketBettingTypes": ["ASIAN_HANDICAP_DOUBLE_LINE"]}, "maxResults": "200", "marketProjection": ["RUNNER_DESCRIPTION", "RUNNER_METADATA"]}'
-        individual_filter = '{"filter":{ "eventIds": ["33464179"], "maxResults": "200", "marketProjection": ["RUNNER_DESCRIPTION", "RUNNER_METADATA"]}'
+        print(output)
+
+        LEEDS_PORTSMOUTH_ID = 33375220
+
+        # response = requests.post(list_competitions_endpoint, data=football_and_weekend_filter, headers=headers)
+        # print(json.dumps(json.loads(response.text), indent=3))
+
+        markets_for_a_game_filter = json.dumps({
+            "filter": {
+                "eventIds": [LEEDS_PORTSMOUTH_ID],
+                # "marketBettingTypes": ["ASIAN_HANDICAP_DOUBLE_LINE"],
+            },
+            "maxResults": 200,
+            "marketProjection": [
+                "RUNNER_DESCRIPTION",
+                "RUNNER_METADATA"
+            ]
+        })
         list_market_catalogue_endpoint = url_prefix + 'listMarketCatalogue/'
 
-        response = requests.post(list_market_catalogue_endpoint, data=individual_filter, headers=headers)
+        response = requests.post(list_market_catalogue_endpoint, data=markets_for_a_game_filter, headers=headers)
         print(json.dumps(json.loads(response.text), indent=3))
 
-        market_filter = '{"marketIds":["1.231381167"],"priceProjection":{"priceData":["EX_BEST_OFFERS"], "keyLineDescription": true}}'
+        book_filter = json.dumps({
+            "marketIds": ["1.230168138"],
+            "priceProjection": {
+                "priceData": ["EX_BEST_OFFERS"],
+                "keyLineDescription": True
+            }
+        })
         list_market_book_endpoint = url_prefix + 'listMarketBook/'
 
-        response = requests.post(list_market_book_endpoint, data=market_filter, headers=headers)
+        response = requests.post(list_market_book_endpoint, data=book_filter, headers=headers)
         print(json.dumps(json.loads(response.text), indent=3))
 
     else:
