8d792540f31c47c9861d0881b7d3859ffd5a4fb6
Michael Davis <745953+mbd0910@users.noreply.github.com>
2024-10-07 16:14:23 +0100

    Add Game and GameTeam to admin pages

--- analytics/football/admin.py
+++ analytics/football/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from football.models import Confederation, Country, Team, TeamExternalLink
+from football.models import Confederation, Country, Game, GameTeam, Team, TeamExternalLink
 
 class ConfederationAdmin(admin.ModelAdmin):
     list_display = ('code', 'name')
@@ -9,6 +9,14 @@ class ConfederationAdmin(admin.ModelAdmin):
 class CountryAdmin(admin.ModelAdmin):
     list_display = ('name', 'code', 'confederation')
     ordering = ['name']
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('name', 'kickoff', 'stage', 'finished', 'venue')
+    ordering = ['kickoff']
+
+class GameTeamAdmin(admin.ModelAdmin):
+    list_display = ('number', 'game', 'game__name', 'team', 'half_time_score', 'full_time_score', 'after_extra_time_score')
+    ordering = ['id', 'number']
 
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('name', 'short_name', 'code', 'gender', 'team_type', 'country', 'confederation')
@@ -20,5 +28,7 @@ class TeamExternalLinkAdmin(admin.ModelAdmin):
 
 admin.site.register(Confederation, ConfederationAdmin)
 admin.site.register(Country, CountryAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(GameTeam, GameTeamAdmin)
 admin.site.register(Team, TeamAdmin)
 admin.site.register(TeamExternalLink, TeamExternalLinkAdmin)
