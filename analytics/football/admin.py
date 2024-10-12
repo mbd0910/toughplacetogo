1a8ea24975d05c348eb0c22b39b25567737bc5a4
Michael Davis <745953+mbd0910@users.noreply.github.com>
2024-10-12 08:25:22 +0100

    Add stage and points deduction to admin pages

--- analytics/football/admin.py
+++ analytics/football/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from football.models import Competition, CompetitionExternalLink, Confederation, Country, Game, GameTeam, Team, TeamExternalLink
+from football.models import Competition, CompetitionExternalLink, Confederation, Country, Game, GameTeam, Stage, StagePointsDeduction, Team, TeamExternalLink
 
 class CompetitionAdmin(admin.ModelAdmin):
     list_display = ('name', 'gender', 'competition_type', 'country', 'confederation')
@@ -24,6 +24,13 @@ class GameTeamAdmin(admin.ModelAdmin):
     list_display = ('number', 'game', 'game__name', 'team', 'half_time_score', 'full_time_score', 'after_extra_time_score')
     ordering = ['id', 'number']
 
+class StageAdmin(admin.ModelAdmin):
+    list_display = ('season', 'name')
+    ordering = ['season', 'name']
+
+class StagePointsDeductionAdmin(admin.ModelAdmin):
+    list_display = ('stage__season', 'stage', 'team', 'deduction')
+
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('name', 'short_name', 'code', 'gender', 'team_type', 'country', 'confederation')
     ordering = ['name']
@@ -38,5 +45,7 @@ admin.site.register(Confederation, ConfederationAdmin)
 admin.site.register(Country, CountryAdmin)
 admin.site.register(Game, GameAdmin)
 admin.site.register(GameTeam, GameTeamAdmin)
+admin.site.register(Stage, StageAdmin)
+admin.site.register(StagePointsDeduction, StagePointsDeductionAdmin)
 admin.site.register(Team, TeamAdmin)
 admin.site.register(TeamExternalLink, TeamExternalLinkAdmin)
