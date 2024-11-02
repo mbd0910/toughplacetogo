3e62e7648c2e24779ca88bb4110456227ae034ad
Michael Davis <745953+mbd0910@users.noreply.github.com>
2024-11-02 18:09:50 +0000

    Move rating calculation to strategy class so to easily switch out rating style (#18)

--- analytics/football/modelling/strength_of_schedule_calculator.py
+++ analytics/football/modelling/strength_of_schedule_calculator.py
@@ -9,6 +9,7 @@ from football.models import Team
 def calculate_opponents_ranking(team: Team,
                                 game_chooser: GameChooser,
                                 rows_by_team: Dict[Team, LeagueTableRow],
+                                team_ratings: Dict[Team, float],
                                 remove_games_against: Team = None):
     if remove_games_against is None:
         games = game_chooser.choose_games(rows_by_team[team])
@@ -22,19 +23,21 @@ def calculate_opponents_ranking(team: Team,
         return None
 
     opponent_rankings = [
-        team_rating(rows_by_team[game_pov.opposition]) * home_advantage_multiplier(game_pov)
+        team_ratings[game_pov.opposition] * home_advantage_multiplier(game_pov)
         for game_pov in games
     ]
     return mean(opponent_rankings)
 
 def calculate_opponents_opponents_ranking(team: Team,
                                           game_chooser: GameChooser,
-                                          rows_by_team: Dict[Team, LeagueTableRow]):
+                                          rows_by_team: Dict[Team, LeagueTableRow],
+                                          team_ratings: Dict[Team, float]):
     opponents_opponents_rankings = [
         calculate_opponents_ranking(
             game_pov.opposition,
             game_chooser,
             rows_by_team,
+            team_ratings,
             team
         )
         for game_pov in game_chooser.choose_games(rows_by_team[team])
@@ -48,9 +51,9 @@ def remove_games_against_team(games: List[GamePOV], team: Team):
 def home_advantage_multiplier(game_pov: GamePOV):
     return 1.25 if not game_pov.is_home else 1.0
 
-def team_rating(league_table_row: LeagueTableRow):
-    return 0.7 * league_table_row.xg_difference_per_game() + 0.3 * league_table_row.goal_difference_per_game()
-
-def calculate_strength_of_schedule(team: Team, game_chooser: GameChooser, rows_by_team: Dict[Team, LeagueTableRow]):
-    return ((2/3) * calculate_opponents_ranking(team, game_chooser, rows_by_team)
-            + (1/3) * calculate_opponents_opponents_ranking(team, game_chooser, rows_by_team))
+def calculate_strength_of_schedule(team: Team,
+                                   game_chooser: GameChooser,
+                                   rows_by_team: Dict[Team, LeagueTableRow],
+                                   team_ratings: Dict[Team, float]):
+    return ((2/3) * calculate_opponents_ranking(team, game_chooser, rows_by_team, team_ratings)
+            + (1/3) * calculate_opponents_opponents_ranking(team, game_chooser, rows_by_team, team_ratings))
---
+++ analytics/football/tests/test_weighted_goal_difference_team_rating_strategy.py
@@ -0,0 +1,58 @@
+from django.test import TestCase
+
+from football.league_table import GamePOV, LeagueTableRow
+from football.modelling.game_chooser import ResultsChooser
+from football.modelling.strength_of_schedule_calculator import calculate_strength_of_schedule, \
+    calculate_opponents_ranking, calculate_opponents_opponents_ranking
+from football.models import Team
+from football.modelling.team_rating_strategy import WeightedGoalDifferenceTeamRatingStrategy
+
+
+class StrengthOfScheduleIntegrationTestCase(TestCase):
+    team_1 = Team(id=1, name='Team 1')
+    team_2 = Team(id=2, name='Team 2')
+    team_3 = Team(id=3, name='Team 3')
+    team_4 = Team(id=4, name='Team 4')
+    team_1_row = LeagueTableRow(team=team_1)
+    team_2_row = LeagueTableRow(team=team_2)
+    team_3_row = LeagueTableRow(team=team_3)
+    team_4_row = LeagueTableRow(team=team_4)
+    team_1_row.add_result(GamePOV(team_1, team_2, is_home=True, scored=1, conceded=1))
+    team_2_row.add_result(GamePOV(team_2, team_1, is_home=False, scored=1, conceded=1))
+    team_3_row.add_result(GamePOV(team_3, team_1, is_home=True, scored=2, conceded=1))
+    team_1_row.add_result(GamePOV(team_1, team_3, is_home=False, scored=1, conceded=2))
+    team_2_row.add_result(GamePOV(team_2, team_3, is_home=True, scored=0, conceded=1))
+    team_3_row.add_result(GamePOV(team_3, team_2, is_home=False, scored=1, conceded=0))
+    team_4_row.add_result(GamePOV(team_4, team_2, is_home=True, scored=3, conceded=4))
+    team_2_row.add_result(GamePOV(team_2, team_4, is_home=False, scored=4, conceded=3))
+    team_1_row.xg = 0.3
+    team_1_row.xg_against = 0.2
+    team_2_row.xg = 0.4
+    team_2_row.xg_against = 0.2
+    team_3_row.xg = 0.8
+    team_3_row.xg_against = 0.3
+    team_4_row.xg = 0.7
+    team_4_row.xg_against = 0.8
+
+    rows_by_team = {
+        team_1: team_1_row,
+        team_2: team_2_row,
+        team_3: team_3_row,
+        team_4: team_4_row,
+    }
+
+    weighted_goal_difference_team_rating_strategy = WeightedGoalDifferenceTeamRatingStrategy(0.7)
+    team_ratings = weighted_goal_difference_team_rating_strategy.calculate_team_ratings(rows_by_team)
+
+    results_chooser = ResultsChooser()
+
+    def test_team_ratings(self):
+        team_1_rating = self.team_ratings[self.team_1]
+        team_2_rating = self.team_ratings[self.team_2]
+        team_3_rating = self.team_ratings[self.team_3]
+        team_4_rating = self.team_ratings[self.team_4]
+
+        self.assertAlmostEqual(-0.115, team_1_rating)
+        self.assertAlmostEqual(0.0467, team_2_rating, 4)
+        self.assertAlmostEqual(0.475, team_3_rating)
+        self.assertAlmostEqual(-0.37, team_4_rating)
